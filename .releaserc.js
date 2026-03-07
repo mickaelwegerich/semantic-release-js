58e5dcf07157c78f1d39a1416113934f9da4abe2
Mickael Wegerich <mickael.wegerich@3lights-technology.com>
2026-03-07 10:29:06 +0100

    feat: test semantic

---
+++ .releaserc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  branches: ['master', 'main'],
+  verifyConditions: ['@semantic-release/changelog', '@semantic-release/git', '@semantic-release/github'],
+  prepare: [
+    '@semantic-release/changelog',
+    {
+      path: '@semantic-release/git',
+      message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+    },
+  ],
+  publish: ['@semantic-release/github'],
+  success: false,
+  fail: false,
+  plugins: [
+    ['@semantic-release/commit-analyzer', {
+      preset: 'conventionalcommits',
+      releaseRules: [
+        { type: 'fix', release: 'patch' },
+        { type: 'feat', release: 'minor' },
+        { breaking: true, release: 'major' },
+        { type: '*', release: 'patch' },
+      ],
+    }], ['@semantic-release/release-notes-generator', {
+      preset: 'conventionalcommits',
+      presetConfig: {
+        types: [
+          { type: 'feat', section: 'Features' },
+          { type: 'fix', section: 'Bug Fixes' },
+          { type: 'tech', section: 'Others', hidden: false },
+        ],
+      },
+    }],
+  ],
+}
